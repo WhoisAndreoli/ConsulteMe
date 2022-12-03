fc97befb06516c86eb91307e51fce59f8108aaa7
WhoisAndreoli <andre_oliiveira97@hotmail.com>
2022-12-03 01:48:20 -0300

    first commit

---
+++ src/main/java/com/example/consulteme/ConsulteMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.consulteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulteMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulteMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consulteme/ConsulteMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consulteme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulteMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
